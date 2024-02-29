b44d3c658b02489010697785bd2bef93edd37a15
sachinthacham <sachinthachamindu26@gmail.com>
2024-02-29 22:03:49 +0530

    Your descriptive commit message here

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Routes,Route} from 'react-router-dom'
+import Home from './Pages/Home'
+import Store from './Pages/Store'
+import About from './Pages/About'
+import {Container} from "react-bootstrap"
+import Navbar from './Components/Navbar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+  <Navbar/>
+   <Container className='mb-4'>
+    <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/store' element={<Store/>}/>
+        <Route path='/about' element={<About/>}/>
+      </Routes>
+   </Container>
+   </>  
+   
   );
 }
 
---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Container,Navbar as Navbarbs}  from 'react-bootstrap'
+
+function Navbar() {
+  return (
+   <Navbarbs className='bg-white shadow-sm mb-3'>
+    <Container>Nav</Container>
+   </Navbarbs>
+  )
+}
+
+export default Navbar
